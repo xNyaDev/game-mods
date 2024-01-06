142382062a9e26aac5113a4ee57bb5e06b538487
xNyaDev <xnyadev@pm.me>
2024-01-06 16:39:21 +0100

    build - switch Rally Trophy to use dinput8 as modloader

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 
-cargo +nightly run --target=i686-pc-windows-msvc -p xnya_modloader_gen -- version.dll --out target/xnya_modloader_version_i686
+cargo +nightly run --target=i686-pc-windows-msvc -p xnya_modloader_gen -- dinput8.dll --out target/xnya_modloader_dinput8_i686
 
-cd target\xnya_modloader_version_i686
+cd target\xnya_modloader_dinput8_i686
 cargo +nightly build --target=i686-pc-windows-msvc --release
 cd ..\..
 
@@ -15,8 +15,8 @@ mkdir "dist\Rally Trophy"
 
 copy target\x86_64-pc-windows-msvc\release\xnya_console_enabler.exe dist
 
-copy target\xnya_modloader_version_i686\target\i686-pc-windows-msvc\release\xnya_modloader.dll "dist\Rally Trophy\version.dll"
-copy target\xnya_modloader_version_i686\target\xnya_modloader.toml "dist\Rally Trophy"
+copy target\xnya_modloader_dinput8_i686\target\i686-pc-windows-msvc\release\xnya_modloader.dll "dist\Rally Trophy\dinput8.dll"
+copy target\xnya_modloader_dinput8_i686\target\xnya_modloader.toml "dist\Rally Trophy"
 copy target\i686-pc-windows-msvc\release\xnya_rallytrophy_cryptutil.dll "dist\Rally Trophy"
 copy target\xnya_rallytrophy_cryptutil.toml "dist\Rally Trophy"
 
